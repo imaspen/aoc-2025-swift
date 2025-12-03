cad5b2f322a2fbf9e8973511165d640dbb5b4872
Aspen <4019381+imaspen@users.noreply.github.com>
2025-12-03 11:38:30 +0000

    day 2 part 2

--- Sources/Day02/Day02.swift
+++ Sources/Day02/Day02.swift
@@ -7,16 +7,21 @@ public struct Day02: Day {
 	public func part1(input: String) async -> String {
 		let ranges = ClosedRange<Int>.parseList(from: input)
 		let invalid = ranges.map { range in
-			range.filter { !checkValidity(of: $0) }.reduce(0, +)
-		}.reduce(0, +)
+			range.filter { !checkValidity(of: $0) }.sum()
+		}.sum()
 		return String(invalid)
 	}
 
 	public func part2(input: String) async -> String {
-		return ""
+		let ranges = ClosedRange<Int>.parseList(from: input)
+		let invalid = ranges.map { range in
+			let x = range.filter { !checkValidity2(of: $0) }
+			return x.sum()
+		}.sum()
+		return String(invalid)
 	}
 
-	func checkValidity(of value: Int) -> Bool {
+	private func checkValidity(of value: Int) -> Bool {
 		let base = (floor(log10(Double(value))) + 1.0) / 2.0
 		if floor(base) != base {
 			return true
@@ -28,6 +33,36 @@ public struct Day02: Day {
 		let secondHalf = value % divisor
 
 		return firstHalf != secondHalf
+	}
+
+	private func checkValidity2(of value: Int) -> Bool {
+		let len = Int(floor(log10(Double(value)))) + 1
+		let maxSubLen = len / 2
+
+		if maxSubLen == 0 {
+			return true
+		}
+
+		for i in 1...maxSubLen {
+			// skip checking lengths that don't divide evenly
+			if (len % i) != 0 {
+				continue
+			}
+
+			let divisor = Int(pow(10.0, Double(i)))
+			let firstPart = value % divisor
+			var rest = value / divisor
+
+			while rest > 0, (rest % divisor) == firstPart {
+				rest /= divisor
+			}
+
+			if rest == 0 {
+				return false
+			}
+		}
+
+		return true
 	}
 }
 
--- Tests/Day02.swift
+++ Tests/Day02.swift
@@ -12,4 +12,15 @@ import struct Day02.Day02
 		let result = await day.part1(input: contents)
 		#expect(result == "1227775554")
 	}
+
+	@Test func part2() async throws {
+		let result = await day.part2(input: contents)
+		#expect(result == "4174379265")
+	}
+
+	@Test func part2EdgeCase() async throws {
+		let edgeCaseInput = "10101-10101"
+		let result = await day.part2(input: edgeCaseInput)
+		#expect(result == "0")
+	}
 }
